df1028e86277d050b156188504e770ce6d605114
Davi-santos16 <davicastro213@gmail.com>
2026-08-05 16:18:08 -0300

    feat: add error handling middleware and create routes index file

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import { errorHandling } from "./middlewares/error-handling"
 
 const app = express()
 const PORT = 3333
+
+app.use(errorHandling)
 
 
 app.listen(PORT, () =>{
