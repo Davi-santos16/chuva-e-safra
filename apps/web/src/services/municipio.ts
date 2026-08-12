b431ad78cb5b985761002a625ac6b09ae5026412
danielgfsdev <eudaniel.dev07@gmail.com>
2026-08-12 20:05:12 -0300

    feat: Renomeia função para obter todos os municípios e organiza o código

--- apps/web/src/services/municipio.ts
+++ apps/web/src/services/municipio.ts
@@ -1,7 +1,21 @@
 import { api } from "@/api/config"
 
-export async function municipio() {
+export async function Allmunicipios() {
   const  response  = await api.get('/municipios/todos')
+  console.log('municipio', response.data)
+
+  return response.data
+}
+
+export async function municipios() {
+  const  response  = await api.get('/municipios')
+  console.log('municipio', response.data)
+
+  return response.data
+}
+
+export async function regioes() {
+  const  response  = await api.get('/municipios/regioes-imediatas')
   console.log('municipio', response.data)
 
   return response.data
